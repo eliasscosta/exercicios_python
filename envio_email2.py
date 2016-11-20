9e3dafe40f315efe8a41c25426b9e47305f0f5cc
Elias Souza <eliasscosta@gmail.com>
2016-11-20 19:02:03 -0200

    new envio_email

---
+++ envio_email2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import smtplib
+from smtplib import SMTPException
+
+try:
+     smtp = smtplib.SMTP('smtp.gmail.com', 587)
+     smtp.ehlo()
+     smtp.starttls()
+     smtp.ehlo()
+     smtp.login('seuemail@domain.com', 'suasenha')
+ 
+
+
+
+
+     de = 'eliasscosta@gmail.com'
+     para = raw_input('Digite o email de quem vai receber: ')
+     subject_text = 'Casa comigo?'
+     message_text = 'Amor da minha vida!!!\nEu te amo!!!\n\nCasa comigo?'
+     msg = 'From: %s\nTo: %s\nSubject: %s\n%s' % (de,para,subject_text, message_text)
+    
+
+
+     smtp.sendmail(de, para, msg)
+     print "Email enviado com sucesso"
+     smtp.close()
+    
+except SMTPException, error:
+   print "Error: unable to send email: \n", str(error)
